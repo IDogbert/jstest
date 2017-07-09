32dcebd42fa5c9029bc5c925fc615634b528feac
IDogbert <chriswhite8@me.com>
2017-07-09 09:40:08 -0700

    Object Literals

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-"" 
+var Tony = {
+    firstname: "Tony",
+    lastname: "Alicea",
+    address: {
+        street: "111 Main St.",
+        city: "New York",
+        state: "NY"
+    }
+
+};
+
+function greet(person){
+    console.log('Hi ' + person.firstname);
+}
+
+greet(Tony);
+
+greet({firstname: 'Mary'});
